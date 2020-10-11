86e472742dcb6a2ab8d5be110fbd83fe135da03b
Héber Silvério <hebersilverio@gmail.com>
2020-10-11 20:23:55 -0300

    button colors

--- js/init.js
+++ js/init.js
@@ -537,7 +537,7 @@ function arlo_tm_animate_text() {
     var animateSpan = jQuery('.arlo_tm_animation_text_word');
 
     animateSpan.typed({
-        strings: ["Websites", "Software", "Mobile Applications", "APIs", "Databases", "Full Stack"],
+        strings: ["Websites", "JavaScript", "CSS", "WordPress", "HTML", "MySQL"],
         loop: true,
         startDelay: 1e3,
         backDelay: 2e3
