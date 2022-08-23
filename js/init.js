5a29c1bb2fbf20459914bbfb2f83f1ecc118735b
Heber Silverio <hebersilverio@gmail.com>
2022-08-23 09:56:15 -0300

    Atualização das ferramentas

--- js/init.js
+++ js/init.js
@@ -537,7 +537,7 @@ function arlo_tm_animate_text() {
     var animateSpan = jQuery('.arlo_tm_animation_text_word');
 
     animateSpan.typed({
-        strings: ["React","SCRUM", "JavaScript", "CSS3", "TypeScript", "HTML5", "BootStrap", "Node"],
+        strings: ["React","SCRUM", "JavaScript", "CSS3", "TypeScript", "HTML5", "Bootstrap", "Node"],
         loop: true,
         startDelay: 1e3,
         backDelay: 2e3
